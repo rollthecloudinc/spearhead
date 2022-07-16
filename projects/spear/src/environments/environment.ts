2a13649e7075bea1062005c0d114331ae18548e9
druid <angular.druid@gmail.com>
2022-07-15 23:48:54 -0400

    Rebuild dev after adding ssh deploy key

--- projects/spear/src/environments/environment.ts
+++ projects/spear/src/environments/environment.ts
@@ -102,7 +102,7 @@ export const environment = {
   }
 };
 
-const x = 'xxxxxxxxxxxxxxxxxxxxxxxsxxxx';
+const x = 'xxxxxxxxxxxxxxxxxxxxxxsxxxx';
 
 /*
  * For easier debugging in development mode, you can import the following file

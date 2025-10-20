32512137951aa443568904ebc8763f0ae0a26e55
tzmijewski <angular.druid@gmail.com>
2025-10-20 18:49:15 -0400

    Upgrade to angular 17 using cli.

--- projects/spear/src/app/app.module.ts
+++ projects/spear/src/app/app.module.ts
@@ -42,7 +42,7 @@ import { DeityModule } from '@rollthecloudinc/deity';
 import { LoopModule } from '@rollthecloudinc/loop';
 import { RenderModule } from '@rollthecloudinc/render';
 import { FormsModule as DruidFormsModule } from '@rollthecloudinc/forms';
-import { TransferHttpCacheModule } from '@nguniversal/common';
+import { TransferHttpCacheModule } from '@angular/ssr';
 import { AlienaliasModule, AlienaliasSettings, ALIENALIAS_SETTINGS } from '@rollthecloudinc/alienalias';
 import { OutsiderModule } from '@rollthecloudinc/outsider';
 import { TractorbeamModule } from '@rollthecloudinc/tractorbeam';
